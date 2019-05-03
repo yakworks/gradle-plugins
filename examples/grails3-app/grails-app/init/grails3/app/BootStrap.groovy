78e43839bb8cf9af75153c6497c59fc9096bb78c
Joshua <joshdev@9ci.com>
2019-05-03 03:47:28 -0600

    add grails3 app example update gradle wrapper

---
+++ examples/grails3-app/grails-app/init/grails3/app/BootStrap.groovy
@@ -0,0 +1,13 @@
+/* Copyright 2019. 9ci Inc. Licensed under the Apache License, Version 2.0 */
+package grails3.app
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
